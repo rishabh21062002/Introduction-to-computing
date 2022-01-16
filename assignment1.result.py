14278ff4050a74c7bb4bc29dadfad9f7227be83e
rishabh21062002 <97827403+rishabh21062002@users.noreply.github.com>
2022-01-16 16:21:28 +0530

    Add files via upload

---
+++ assignment1.result.py
@@ -0,0 +1,22 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============ RESTART: C:/Users/jk/Pictures/pythonidle/assignment1.py ===========
+ENTER FIRST NUMBER: 23
+ENTER SECOND NUMBER: 3
+ENTER THIRD NUMBER: 45
+the average of these three numbers is: 23.666666666666668
+ ENTER THE GROSS INCOME TO THE NEAREST PENNY: 1000000
+ ENTER NUMBER OF DEPENDENTS: 5
+ THE TAX IS : 195000.0
+ THE BELOW IS THE LIST OF DIFFERENT DATA TYPE VALUES : 
+[21105105, 'saket', 'male', 'ECE', 9.8]
+ENTER MARKS OF STUDENT 1: 43
+ENTER MARKS OF STUDENT 2: 54
+ENTER MARKS OF STUDENT 3: 75
+ENTER MARKS OF STUDENT 4: 99
+ENTER MARKS OF STUDENT 5: 33
+[33, 43, 54, 75, 99]
+['RED', 'GREEN', 'WHITE', 'PINK', 'YELLOW']
+['RED', 'GREEN', 'WHITE', 'PURPLE', 'YELLOW']
+>>> 
