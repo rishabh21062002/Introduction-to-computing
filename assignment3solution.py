62b198a3b12cea64ca93cbe4bb3acf888f85aef7
rishabh21062002 <97827403+rishabh21062002@users.noreply.github.com>
2022-02-14 14:21:43 +0530

    Add files via upload

---
+++ assignment3solution.py
@@ -0,0 +1,84 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========== RESTART: C:/Users/Lenovo/Documents/Downloads/assignment3.py =========
+THE SOLUTION OF QUESTION1 :
+
+Enter the string: rishabh
+The occurence(s) of:
+		"r"    is/are   1 
+		"i"    is/are   1 
+		"s"    is/are   1 
+		"h"    is/are   2 
+		"a"    is/are   1 
+		"b"    is/are   1 
+
+THE SOLUTION OF QUESTION2 :
+
+Day - 2
+Month - 2
+Year - 2000
+Next Date is: 3/02/2000
+
+THE SOLUTION OF QUESTION3 :
+
+Enter the list: 2,3,4
+Output: [(2, 4), (3, 9), (4, 16)]
+
+The solution of Question 4 is:
+
+Enter the grade point of the student: 9
+Your Grade is 'A' and Excellent Performance
+ABCDEFGHIJK
+ ABCDEFGHI 
+  ABCDEFG  
+   ABCDE   
+    ABC    
+     A     
+
+The solution of Question 6 is:
+
+Enter the name of the student: rishabh
+Enter the SID of rishabh: 21105114
+
+You have entered 1 value(s) till now
+Do you want to enter more data? y
+Enter the name of the student: saket
+Enter the SID of saket: 21105105
+
+You have entered 2 value(s) till now
+Do you want to enter more data? y
+Enter the name of the student: anjali
+Enter the SID of anjali: 21105113
+
+You have entered 3 value(s) till now
+Do you want to enter more data? n
+
+a. Student Details:
+The SID of [1mrishabh[0m is [1m21105114[0m
+The SID of [1msaket[0m is [1m21105105[0m
+The SID of [1manjali[0m is [1m21105113[0m
+
+b. Student Details (sorted with respect to names):
+The SID of [1manjali[0m is [1m21105113[0m
+The SID of [1mrishabh[0m is [1m21105114[0m
+The SID of [1msaket[0m is [1m21105105[0m
+
+c. Student Details (sorted with respect to SIDs):
+The SID of [1msaket[0m is [1m21105105[0m
+The SID of [1manjali[0m is [1m21105113[0m
+The SID of [1mrishabh[0m is [1m21105114[0m
+
+d. Enter the SID of the student: 21105114
+The name of student whose SID is 21105114 is [1mrishabh[0m
+ENTER A NUMBER : 9
+1 1 2 3 5 8 13 21 34 
+Average is : 9.777777777777779
+
+The solution of Question 8 is:
+
+a. Set of all elements that are in Set1 and Set2 but not both: {1, 3, 5, 6, 8}
+b. Set of all elements that are in only one of the three sets Set1, Set2 and Set3: {17, 3, 6, 8, 9, 13}
+c. Set of elements that are in exactly two of the sets Set1, Set2 and Set3: {1, 2, 4, 5}
+d. Set of all integers in the range 1 to 10 that are not in Set1: {6, 7, 8, 9, 10}
+e. Set of all integers in the range 1 to 10 that are not in Set1, Set2 and Set3: {10, 7}
